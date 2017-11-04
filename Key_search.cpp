b38692838ae8c963964e0465478721b272a91f1a
Payas Bhrigu <bhrigu.payas10@gmail.com>
2017-11-04 19:52:35 +0530

    Update Key_search.cpp

--- Key_search.cpp
+++ Key_search.cpp
@@ -1,130 +1,38 @@
 #include<bits/stdc++.h>
-
+#include<string.h>
 using namespace std;
-
-int a=-1;
-
-int OpCodes(char s1[100][100],char s2[])
+bool icompare_pred(unsigned char a, unsigned char b)
 {
-       
-       a++;
-       
-       
-        int i=0,j=0,k=0,count1 = 0, count2 = 0,count=0,flag=-2;  
-     
-        while (s1[a][count1] != '\0')
-
-                count1++;                       //Calculating length of s1 (Line from file)
-
-        while (s2[count2] != '\0')
-
-                count2++;                       //Calculating length of s2  (Token)
-        
-       while(s1[a])
-       {
-                if(count2<count1)                       //The length of substring is always less than the compared string
-                {
-
-                        for (i = 0; i <= count1 - count2; i++)
-
-                        {
-
-                                for (j = i; j < i + count2; j++)
-
-                                {
-
-                                        flag = 1;
-
-                                        if (s1[j][k] != s2[j - i])
-
-                                         {
-
-                                                flag = 0;
-
-                                                break;
-
-                                         }
-                                         k++;
-
-                                }
-
-                                if (flag == 1)
-
-                                count++;
-
-                        }   
-                }
-                a++;
-    }
-    return count;
+    return std::tolower(a) == std::tolower(b);
 }
-    
-void GenerateTokens(char token[])
+
+bool icompare(string const& a, string const& b)
 {
-        
-        cout<<"Enter the token to be used in this code : ";
-        gets(token);
-        strcat(token," ");
-}    
-        
+    if (a.length()==b.length()) {
+        return std::equal(b.begin(), b.end(),
+                           a.begin(), icompare_pred);
+    }
+    else {
+        return false;
+    }
+}
 int main()
-{
-        ifstream fin("Koderunners.kdr");
-        char token[100],word[100];
-        int i=0,d=0;
-         
-
-        GenerateTokens(token);
-
-        
-        char linearr[100][100];
-        
-        int countOccur[100];
-        for( d=0;d<100;d++)
-                countOccur[d]=0;
-        
-        
-        
-        cout<<"The input file is : \n";                         // Taking the input from file in each line
-        i=0;
-        while(fin)
-        {        
-        
-               
-                if(fin!='\0')
-                {
-                        fin.getline(word,100,'\n');
-                        strcpy(linearr[i],word);
-                        puts(linearr[i]);
-                        cout<<"\n";
-                }
-                  
-          i++;
-        
-        }  
-        printf("\n\n");      
-        
-
-        d=0;
-        int Sum_of_counts=0;
-                for(int z=0;z<i-1;z++)
-                {
-                        
-                     countOccur[d]= OpCodes(linearr,token);             //Counting how many times token has occurred in file
-                     Sum_of_counts+=countOccur[d];
-                     d++;
-                      
-                 } 
-
-                        
-                        
-        
-        if(Sum_of_counts==0)
-                    cout<<"Error ! Some words other than"<<token <<" have appeared !";          //Printing desired output
-        else
-                    cout<<token<<" has appeared in the file "<<Sum_of_counts<<" times"; 
-        
-               
-        fin.close();
-        return 0;
+{	ifstream fin("/home/kiit/Payas/General/Koderunners.txt");
+	cout<<"Enter String:\n";
+	string s;	
+	cin>>s;
+	string ch;
+	int c=0;
+	while(fin>>ch)
+	{	//fin.getline(ch,100);
+		//fin>>ch;
+		//if(strcmpi(s,ch))
+		//if (boost::iequals(s,ch))
+		if(icompare(s,ch))
+		++c;
+		cout<<ch;
+	}
+	cout<<'\n'<<c<<'\n';
+	//fin.close();
+	return 0;
 }
